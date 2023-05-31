38e85ed90b6c4ee20ce53eabf664805cacb160b5
Marc González <marcgoam@gmail.com>
2023-05-31 09:32:57 +0200

    Third test passed

--- contracts/Game3.sol
+++ contracts/Game3.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.17;
 
 contract Game3 {
-  uint8 y = 210;
+    uint8 y = 210;
 
-  event Winner(address winner);
+    event Winner(address winner);
 
-  function win(uint8 x) public {
-    unchecked {
-        uint8 sum = x + y;
-        require(sum == 255, "Incorrect argument passed in!");
+    function win(uint8 x) public {
+        unchecked {
+            uint8 sum = x + y;
+            require(sum == 255, "Incorrect argument passed in!");
+        }
+        emit Winner(msg.sender);
     }
-    emit Winner(msg.sender);
-  }
 }
--- scripts/win.js
+++ scripts/win.js
@@ -1,16 +1,14 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-const contractName = "Game2";
+const gameAddr = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+const contractName = "Game3";
 
 async function main() {
   // attach to the game
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
   // do whatever you need to do to win the game here:
-  await game.setX(1);
-  await game.setY(49);
 
-  const tx = await game.win();
+  const tx = await game.win(45);
 
   // did you win? Check the transaction receipt!
   // if you did, it will be in both the logs and events array
