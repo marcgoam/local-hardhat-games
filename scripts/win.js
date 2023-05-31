92e30b943d8bec2f08f101d11f5e782024e4abfd
Marc González <marcgoam@gmail.com>
2023-05-31 09:39:13 +0200

    All tests passed

--- scripts/win.js
+++ scripts/win.js
@@ -1,14 +1,16 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
-const contractName = "Game4";
+const gameAddr = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+const contractName = "Game5";
 
 async function main() {
   // attach to the game
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
   // do whatever you need to do to win the game here:
+  await game.giveMeAllowance(300000);
+  await game.mint(200000);
 
-  const tx = await game.win(56);
+  const tx = await game.win();
 
   // did you win? Check the transaction receipt!
   // if you did, it will be in both the logs and events array
